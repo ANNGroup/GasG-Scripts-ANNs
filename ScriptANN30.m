bbe386daea16911643b8ad3aaa5a87679a1d94e2
ANNGroup <38044426+ANNGroup@users.noreply.github.com>
2018-05-07 21:26:35 -0500

    Update ScriptANN30.m

--- ScriptANN30.m
+++ ScriptANN30.m
@@ -11,7 +11,8 @@
 % 4 GB, and  a Hard drive 500 GB
 % Software requirements: Matlab for Windows v. R2017b (MathWorks, 2017)
 % Programming language: MATLAB
-% Programming file size: 37 kB with 281 lines of code
+% Programming file size: 37 kB with 282 lines of code
+
 %The MIT License (MIT)
 %Copyright (c) 2018 D. Pérez-Zárate, E. Santoyo, A. Acevedo-Anicasio, C. García-López, L. Díaz-González
 
